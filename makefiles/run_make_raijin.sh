bcc5429b60a264d37c5045061409558091df5cdd
rcb547 <ross.c.brodie@icloud.com>
2016-04-28 19:24:03 +1000

    Added makefile for compiling on raijin@nci.org.au

---
+++ makefiles/run_make_raijin.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#Script to load compiler modules and dependent software 
+#on raijin.nci.org.au
+
+module load gcc/5.2.0
+module load openmpi/1.6.3
+module load fftw3/3.3.3
+
+make -f gaforwardmodeltdem.make
+make -f galeisbstdem.make
+make -f garjmcmctdem.make
+
