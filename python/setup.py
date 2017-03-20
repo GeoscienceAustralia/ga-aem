7147decd1a96dfd5705c15fdf5b8762e23cc3292
rcb547 <ross.c.brodie@icloud.com>
2017-03-20 13:53:14 +1100

    Updated metadata in setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,7 @@
 # Setup Script
 # Author: Leon Foks
 # March 10 2017
+# Updated: Ross C Brodie, March 20 2017
 import sys
 import os
 from os.path import join
@@ -21,16 +22,16 @@ setup(name='gatdaem1d',
       packages=['gatdaem1d'],
       scripts=[],
       version=1.0,
-      description='Time domain electromagnetic forward modeller',
-      long_description='',
+      description='Time-domain airborne electromagnetic forward modelling.',
+      long_description='Time-domain airborne electromagnetic forward modelling. Python interface to C++ library for forward model and derivative calculations for airborne electromagnetic (AEM) systems used in geophysics.',
       classifiers=[
         'Development Status :: 0 - Alpha',
-        'License :: OSI Approved :: GPL?',
+        'License :: OSI Approved :: GNU GPL Version 2.0',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Electromagnetic :: forward modelling :: geophysics',
+        'Topic :: electromagnetic :: airborne :: forward modelling :: geophysics',
       ],
-      author='Ross Brodie',
-      author_email='ross.c.brodie@ga.gov.au',
+      author='Ross C Brodie, Geoscience Australia',
+      author_email='ross.c.brodie at ga.gov.au',
       install_requires=[
           'numpy>=1.11',
       ],
