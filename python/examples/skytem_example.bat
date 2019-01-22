222e597306cd1cdfbab1f126dfa50f640d2753b6
rcb547 <ross.c.brodie@icloud.com>
2019-01-22 17:47:54 +1100

    vs2017 compile

--- python/examples/skytem_example.bat
+++ python/examples/skytem_example.bat
@@ -1,7 +1,13 @@
 @echo OFF
 
-set PYTHONPATH=Z:\code\repos\ga-aem\python;%PYTHONPATH%
+REM Add Python3.x installation directory to the PATH variable
+set path=%PYTHON3_ROOT%;%PATH%
+
+REM Add the ga-aem python code to the PYTHONPATH variable
+set PYTHONPATH=%CD%\..;%PYTHONPATH%
 echo %PYTHONPATH%
+
+REM Run the example
 python skytem_example.py
 
 pause
