61368e3d655fcf69bfc29bb5edebba07c1b65056
rcb547 <ross.c.brodie@icloud.com>
2019-02-11 12:11:26 +1100

    Updated VS project files for Windows SDK version 10.0.17134.0

--- examples/thomson-vtem/galeisbstdem/run.bat
+++ examples/thomson-vtem/galeisbstdem/run.bat
@@ -2,9 +2,11 @@
 
 REM Add executable and FFTW directories to yo search path
 REM (Ideally you would add these to your PATH environment variable)
-set path=..\..\..\bin\x64\Release\;%path%
-REM set path=..\..\..\third_party\fftw3.2.2.dlls\64bit;%path%
 
+REM set path=C:\Microsoft_HPC_Pack_2012\Bin;%path%
+REM set path=..\..\..\..\fftw3.2.2.dlls\64bit;%path%
+set path=..\..\..\..\petsc\3.9.4\vs2017\win64_release\lib;%path%
+set path=..\..\..\bin\x64\Release\;%path%
 
 REM Standalone
 REM #galeisbstdem.exe galeisbstdem.con
@@ -13,11 +15,11 @@ REM Use 4 MPI processes
 REM mpiexec -np 4 galeisbstdem.exe galeisbstdem.con
 
 REM Use 4 OpenMP threads
-REM galeisbstdem.exe galeisbstdem.con 4
+galeisbstdem.exe galeisbstdem.con 4
 
 REM If you don't have MPI installed use the non-MPI version
 REM Use 4 OpenMP threads 
-galeisbstdem-nompi.exe galeisbstdem.con 4
+REM galeisbstdem-nompi.exe galeisbstdem.con 4
 
 pause
 
