aed8e192305b9f6f3265630f4072b68f815b95f9
richardt94 <29562583+richardt94@users.noreply.github.com>
2020-08-25 16:14:22 +1000

    Fix misfit reporting in text output: - Use phi_d rather than the unnormalised log-likelihood that is used for the - MH proposals

--- src/garjmcmctdem.cpp
+++ src/garjmcmctdem.cpp
@@ -63,7 +63,7 @@ int main(int argc, char* argv[])
 			I.parsecurrentrecord();
 			I.sample();
 			double stime = I.samplingtime;
-			double norm_mfit = I.LowestMisfit.get_misfit() / double(I.ndata);
+			double norm_mfit = I.LowestMisfit.get_chi2() / double(I.ndata);
 			glog.logmsg("Rec %6lu\t %3lu\t %5lu\t %10lf nmfit=%.1lf stime=%.3lfs\n", I.CurrentRecord, I.flightnumber, I.linenumber, I.fidnumber, norm_mfit, stime);
 		}
 		glog.logmsg("This process finishing at %s\n", timestamp().c_str());
