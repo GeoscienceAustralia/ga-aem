0a9154cfb66054f642f4cfb747eaec75ce75b73b
Ross Brodie <rcb547@users.noreply.github.com>
2021-06-24 09:09:12 +1000

    Changes for new input/outputmanager classes 2

---
+++ src/inputmanager.h
@@ -0,0 +1,263 @@
+/*
+This source code file is licensed under the GNU GPL Version 2.0 Licence by the following copyright holder:
+Crown Copyright Commonwealth of Australia (Geoscience Australia) 2015.
+The GNU GPL 2.0 licence is available at: http://www.gnu.org/licenses/gpl-2.0.html. If you require a paper copy of the GNU GPL 2.0 Licence, please write to Free Software Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+Author: Ross C. Brodie, Geoscience Australia.
+*/
+
+#ifndef _inputmanager_H_
+#define _inputmanager_H_
+
+#include "asciicolumnfile.h"
+#include "fielddefinition.h"
+#if defined HAVE_NETCDF
+	#include "geophysics_netcdf.h"
+#endif
+
+class cASCIIInputManager;
+class cNetCDFInputManager;
+
+class cInputManager {
+
+public:
+	enum class IOType { ASCII, NETCDF, NONE };
+
+protected:	
+	bool AtStart = true;
+	std::string DataFileName;
+	IOType iotype = IOType::NONE;
+	size_t Subsample = 1;
+	size_t Record = 0;
+
+public:
+	cInputManager() {};
+
+	virtual ~cInputManager() {};
+
+	void initialise(const cBlock& b) {		
+		DataFileName = b.getstringvalue("DataFile");
+		fixseparator(DataFileName);
+
+		Subsample = b.getsizetvalue("Subsample");
+		if (!isdefined(Subsample)) { Subsample = 1; }
+	}
+
+	static bool isnetcdf(const cBlock& b) {
+		std::string fname = b.getstringvalue("DataFile");
+		std::string ext = extractfileextension(fname);
+		if (strcasecmp(ext, ".nc") == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	size_t subsamplerate() { return Subsample; }
+
+	virtual bool is_record_valid() { return true; }
+		
+	virtual bool readnextrecord() = 0;
+
+	virtual bool parserecord() { return true; }
+	
+
+	const std::string& datafilename() { return DataFileName; }
+
+	const size_t& record() const { return Record; }
+	
+	template<typename T>
+	bool read(const cFieldDefinition& cd, T& v)
+	{
+		if (cd.definitiontype() == UNAVAILABLE) {
+			v = undefinedvalue(v);
+			return false;
+		}
+		else if (cd.definitiontype() == NUMERIC) {
+			v = (T)cd.numericvalue[0];
+			return true;
+		}
+
+		std::vector<T> vec;
+		bool status = file_read(cd,vec,1);
+		v = vec[0];
+
+		if (iotype != IOType::ASCII) {//Don't flip if ASCII reader as it already does this - to be fixed
+			if (cd.flip) { v = -1 * v; } {
+				cd.applyoperator(v);
+			}
+		}
+		return true;
+	}
+
+	template<typename T>
+	bool read(const cFieldDefinition& cd, std::vector<T>& vec, const size_t n)
+	{
+		vec.resize(n);
+		if (cd.definitiontype() == NUMERIC) {
+			size_t deflen = cd.numericvalue.size();
+			for (size_t i = 0; i < n; i++) {
+				if (deflen == 1)vec[i] = (T)cd.numericvalue[0];
+				else            vec[i] = (T)cd.numericvalue[i];
+			}
+			return true;
+		}
+
+		bool status = file_read(cd, vec, n);
+
+		if (iotype != IOType::ASCII) {//Don't flip if ASCII reader as it already does this - to be fixed
+			for (size_t i = 0; i < vec.size(); i++) {
+				if (cd.flip) { vec[i] = -vec[i]; }
+				cd.applyoperator(vec[i]);
+			}
+		}
+		return true;
+	}
+
+	template<typename T>
+	bool file_read(const cFieldDefinition& cd, std::vector<T>& vec, const size_t n)
+	{		
+		if (iotype == IOType::ASCII) {
+			return ((cASCIIInputManager*)this)->file_read(cd, vec, n);
+		}
+		return ((cNetCDFInputManager*)this)->file_read(cd, vec, n);
+	}	
+};
+
+class cASCIIInputManager : public cInputManager {
+
+private:
+	cAsciiColumnFile AF;
+	static bool contains_non_numeric_characters(const std::string& str)
+	{
+		size_t pos = str.find_first_not_of("0123456789.+-eE ,\t\r\n");
+		if (pos == std::string::npos) return false;
+		else return true;
+	}
+
+public:
+
+	std::string HeaderFileName;
+		
+	cASCIIInputManager(const cBlock& b) {
+		cInputManager::initialise(b);
+		initialise(b);
+	}
+
+	~cASCIIInputManager() {	};
+
+	void initialise(const cBlock& b)
+	{		
+		HeaderFileName = b.getstringvalue("DfnFile");
+		fixseparator(HeaderFileName);		
+		
+		iotype = IOType::ASCII;
+		AF.openfile(DataFileName);
+		if (isdefined(HeaderFileName)) {
+			glog.logmsg(0, "Parsing Input DfnFile %s\n", HeaderFileName.c_str());
+			AF.parse_aseggdf2_header(HeaderFileName);
+		}
+
+		size_t headerlines = b.getsizetvalue("Headerlines");
+		if (!isdefined(headerlines)) { headerlines = 0; }
+		for (size_t k = 0; k < headerlines; k++) {
+			AF.readnextrecord();
+		}				
+	}
+
+	bool is_record_valid() {
+		bool nonnumeric = contains_non_numeric_characters(recordstring());
+		if (nonnumeric) {
+			glog.logmsg("Skipping non-numeric record at line %zu of Input DataFile %s\n", record(), datafilename().c_str());
+			glog.logmsg("\n%s\n\n", recordstring().c_str());
+			return false;
+		}
+		return true;
+	}
+	
+	bool readnextrecord()
+	{
+		bool status = true;
+		if (AtStart == true) {
+			status = AF.readnextrecord();
+			if (status == false)return false;
+			AtStart = false;
+			Record = 0;
+		}
+		else {
+			AF.skiprecords(Subsample - 1);
+			status = AF.readnextrecord();
+			if (status == false) return false;
+			Record += Subsample;
+		}		
+		return true;
+	}
+
+	bool parserecord() {
+		size_t n = AF.parserecord();
+		if (n <= 1) return false;
+		return true;		
+	}
+	
+	const std::string& recordstring() const { return AF.currentrecord_string(); }
+
+	const std::vector<std::string>& fields() const { return AF.currentrecord_columns(); }
+
+	template<typename T>
+	bool file_read(const cFieldDefinition& cd, std::vector<T>& vec, const size_t n)
+	{		
+		bool status = cd.getvalue(AF, vec, n);		
+		return status;	
+	}	
+};
+
+#if defined HAVE_NETCDF
+class cNetCDFInputManager : public cInputManager {
+
+private:	
+	cGeophysicsNcFile NC;
+
+public:
+
+	cNetCDFInputManager(const cBlock& b) {
+		cInputManager::initialise(b);
+		initialise(b);
+	}
+
+	~cNetCDFInputManager() {	};
+
+	void initialise(const cBlock& b)
+	{						
+		glog.logmsg(0, "Opening Input DataFile %s\n", DataFileName.c_str());		
+		iotype = IOType::NETCDF;
+		NC.open(DataFileName, netCDF::NcFile::FileMode::read);		
+	}
+
+	bool readnextrecord()
+	{
+		bool status = true;		
+		if (AtStart == true) {
+			AtStart = false;
+			Record = 0;
+		}
+		else {
+			Record += Subsample;
+			#if defined HAVE_NETCDF
+				if (Record > NC.ntotalsamples()) return false;
+			#endif
+		}				
+		return true;
+	}
+
+	template<typename T>
+	bool file_read(const cFieldDefinition& cd, std::vector<T>& v, const size_t n)
+	{
+		NC.getDataByPointIndex(cd.varname, Record, v);
+		return true;
+	}
+
+};
+#endif
+
+#endif
+
+ 
---
+++ src/outputmanager.h
@@ -0,0 +1,406 @@
+/*
+This source code file is licensed under the GNU GPL Version 2.0 Licence by the following copyright holder:
+Crown Copyright Commonwealth of Australia (Geoscience Australia) 2015.
+The GNU GPL 2.0 licence is available at: http://www.gnu.org/licenses/gpl-2.0.html. If you require a paper copy of the GNU GPL 2.0 Licence, please write to Free Software Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+Author: Ross C. Brodie, Geoscience Australia.
+*/
+
+#ifndef _outputmanager_H_
+#define _outputmanager_H_
+
+#include <list>
+#include "asciicolumnfile.h"
+#include "fielddefinition.h"
+
+
+#if defined _MPI_ENABLED
+	#include "mpi_wrapper.h"
+#endif
+
+#if defined HAVE_NETCDF
+	#include "geophysics_netcdf.h"
+#endif
+
+class cASCIIOutputManager;
+class cNetCDFOutputManager;
+class cOutputManager;
+class cOutputField {
+	
+	public:				
+		std::shared_ptr<cGeophysicsVar> var;
+		std::string name;//name
+		std::string description;//description
+		std::string units;//units	
+		size_t bands = 0;//number of bands
+		nc_type ncstoragetype ;//binary storage tyrpe
+		std::string ncdimname;//dimension names		
+		char notation = 0;//ascii notation form I, F, E
+		size_t width = 0 ;//ascii width
+		size_t precision = 0;//ascii numver of decimals places
+		
+		//cOutputField() {};
+
+		cOutputField(			
+			const std::string& _name,//name
+			const std::string& _description,//description
+			const std::string& _units,//units	
+			const size_t& _bands,//number of bands
+			const nc_type& _ncstoragetype,//binary storage tyrpe
+			const std::string& _ncdimname,//dimension names		
+			const char& _notation,//ascii notation I, F, E
+			const size_t& _width,//ascii width
+			const size_t& _precision//ascii numver of decimals places			
+			)
+		{					
+			name = _name;
+			description = _description;
+			units = _units;
+			bands = _bands;
+			ncstoragetype = _ncstoragetype;
+			ncdimname = _ncdimname;
+			notation = _notation;
+			width = _width;
+			precision = _precision;			
+		}	
+
+		//~cOutputField() {};
+};
+
+class cOutputManager {
+
+public:
+	enum class IOType { ASCII, NETCDF, NONE };
+
+protected:		
+	std::string DataFileName;	
+	IOType iotype = IOType::NONE;	
+	std::list<cOutputField> flist;
+
+public:
+	cOutputManager() {};
+
+	virtual ~cOutputManager() {};
+
+	void initialise(const cBlock& b) {		
+		DataFileName = b.getstringvalue("DataFile");
+		fixseparator(DataFileName);
+		//LogFileName = b.getstringvalue("LogFile");
+		//fixseparator(DataFileName);
+		//Subsample = b.getsizetvalue("Subsample");
+		//if (!isdefined(Subsample)) { Subsample = 1; }
+	}
+
+	static bool isnetcdf(const cBlock& b) {
+		std::string fname = b.getstringvalue("DataFile");
+		std::string ext = extractfileextension(fname);
+		if (strcasecmp(ext, ".nc") == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	const std::string& datafilename() { return DataFileName; }
+
+	virtual bool opendatafile(const std::string& srcfile, const size_t& subsample) = 0;
+	
+	std::shared_ptr<cOutputField> getfield(const std::string& name) {
+		std::shared_ptr<cOutputField> f;
+		for(auto it = flist.begin(); it != flist.end(); it++) {			
+			if (strcasecmp(it->name, name) == 0) {
+				f = std::make_shared<cOutputField>(*it);
+				return f;
+			}
+		}
+		return f;
+	}
+
+	virtual std::shared_ptr<cOutputField> addfield(
+		const std::string& _name,//name
+		const std::string& _description,//description
+		const std::string& _units,//units	
+		const size_t& _bands,//number of bands
+		const nc_type& _ncstoragetype,//binary storage tyrpe
+		const std::string& _ncdimname,//dimension names		
+		const char& _notation,//ascii form I, F, E
+		const size_t& _width,//ascii width
+		const size_t& _precision//ascii numver of decimals places			
+	) = 0;
+
+	virtual void begin_point_output() = 0;
+	virtual void end_point_output() = 0;
+	virtual bool end_first_record() {
+		return true;
+	};
+
+	virtual bool addvar(cOutputField& of) { return true; }
+
+	//Cannot templatize these virtual functions
+	virtual bool write(const int& val, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;
+	virtual bool write(const size_t& val, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;
+	virtual bool write(const float& val, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;
+	virtual bool write(const double& val, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;
+
+	virtual bool write(const std::vector<int>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;	
+	virtual bool write(const std::vector<size_t>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;
+	virtual bool write(const std::vector<float>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;
+	virtual bool write(const std::vector<double>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) = 0;	
+};
+
+class cASCIIOutputManager : public cOutputManager {
+
+private:
+	std::ofstream fstrm;		
+	std::ostringstream buf;
+	cOutputFileInfo OI;	
+	void set_formatflags(std::shared_ptr<cOutputField> of){
+		if (of->notation == 'e' || of->notation == 'E') buf << std::scientific;
+		else buf << std::fixed;
+	}
+
+public:
+
+	std::string HeaderFileName;
+		
+	cASCIIOutputManager(const cBlock& b) {
+		cOutputManager::initialise(b);
+		initialise(b);
+	}
+
+	~cASCIIOutputManager() {	};
+
+	void initialise(const cBlock& b)
+	{		
+		
+	}
+	
+	bool opendatafile(const std::string& srcfile, const size_t& subsample) {	
+		glog.logmsg(0,"Opening Output ASCII DataFile %s\n", DataFileName.c_str());				
+		fstrm.open(DataFileName, std::ofstream::out);		
+		//Outputrecord = 1;		
+		return fstrm.is_open();
+	};
+	
+	std::shared_ptr<cOutputField> addfield(
+		const std::string& _name,//name
+		const std::string& _description,//description
+		const std::string& _units,//units	
+		const size_t& _bands,//number of bands
+		const nc_type& _ncstoragetype,//binary storage tyrpe
+		const std::string& _ncdimname,//dimension names		
+		const char& _notation,//ascii form I, F, E
+		const size_t& _width,//ascii width
+		const size_t& _precision//ascii numver of decimals places			
+	) {
+		std::shared_ptr<cOutputField> f = std::make_shared<cOutputField>(_name, _description, _units, _bands, _ncstoragetype, _ncdimname, _notation, _width, _precision);
+		OI.addfield(_name, _notation, _width, _precision, _bands);
+		OI.setunits(_units);
+		OI.setdescription(_description);
+		return f;
+	}
+
+	void begin_point_output() {		
+		buf.str(std::string());	//empty buffer
+	};
+
+	void end_point_output() {
+
+		buf << std::endl; //Carriage return		
+		fstrm << buf.str() << std::flush; // Write to file
+
+		//OI.lockfields();
+		//if (Outputrecord == 1) {
+		//	write_headers();
+		//}
+		//Outputrecord++;
+	};
+
+	bool end_first_record(){
+		write_headers();
+		return true;
+	}
+
+	void write_headers(){
+		sFilePathParts fpp = getfilepathparts(datafilename());
+		std::string hdrfile = fpp.directory + fpp.prefix + ".hdr";
+		OI.write_simple_header(hdrfile);
+		std::string aseggdffile = fpp.directory + fpp.prefix + ".dfn";
+		OI.write_aseggdf_header(aseggdffile);
+	};
+		
+	bool write(const int& val, const std::shared_ptr<cOutputField> of, const int& pointindex) {		
+		return write_scalar(val, of, pointindex);
+	}
+	bool write(const size_t& val, const std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return write_scalar(val, of, pointindex);
+	}
+	bool write(const float& val, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return write_scalar(val, of, pointindex);
+	}
+	bool write(const double& val, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return write_scalar(val, of, pointindex);
+	}
+
+	bool write(const std::vector<int>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {		
+		return write_vector(vals, of, pointindex);
+	}
+	bool write(const std::vector<size_t>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return write_vector(vals, of, pointindex);
+	}
+	bool write(const std::vector<float>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return write_vector(vals, of, pointindex);
+	}
+	bool write(const std::vector<double>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return write_vector(vals, of, pointindex);
+	}
+
+	template <typename T> 
+	bool write_scalar(const T& val, std::shared_ptr<cOutputField> of, const int& pointindex) {		
+		set_formatflags(of);
+		buf.width(of->width);
+		buf.precision(of->precision);
+		buf << val;		
+		return true;
+	}
+
+	template <typename T>
+	bool write_vector(const std::vector<T>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {		
+		set_formatflags(of);
+		for (size_t i = 0; i < vals.size(); i++) {
+			buf.width(of->width);
+			buf.precision(of->precision);
+			buf << vals[i];
+		}
+		return true;
+	}
+	
+};
+
+#if defined HAVE_NETCDF
+class cNetCDFOutputManager : public cOutputManager {
+
+private:	
+	cGeophysicsNcFile NC;
+
+public:
+
+	cNetCDFOutputManager(const cBlock& b) {
+		cOutputManager::initialise(b);
+		initialise(b);
+	}
+
+	~cNetCDFOutputManager() {	};
+
+	void initialise(const cBlock& b)
+	{						
+		glog.logmsg(0, "Opening Input DataFile %s\n", DataFileName.c_str());		
+		iotype = IOType::NETCDF;		
+	}
+
+	bool opendatafile(const std::string& srcfile, const size_t& subsample) {		
+		//std::vector<std::string> include_varnames = { "proj_client", "flight", "flight_index", "longitude", "latitude", "emz_nonhprg" };
+		//std::vector<std::string> exclude_varnames = { "easting", "northing" };
+		std::vector<std::string> include_varnames;
+		std::vector<std::string> exclude_varnames;
+
+		if (cMpiEnv::world_rank() == 0) {
+			glog.logmsg(0, "Creating Output NetCDF DataFile %s\n", DataFileName.c_str());
+			cGeophysicsNcFile inncfile(srcfile, NcFile::FileMode::read);			
+			cGeophysicsNcFile outncfile(datafilename(),NcFile::FileMode::replace);
+			outncfile.subsample(srcfile, subsample, include_varnames, exclude_varnames);
+		}				
+		cMpiEnv::world_barrier();
+		
+		glog.logmsg(0, "Opening Output NetCDF DataFile %s\n", DataFileName.c_str());
+		NC.open(datafilename(), NcFile::FileMode::write);
+		return true;
+	};
+
+	std::shared_ptr<cOutputField> addfield(
+		const std::string& _name,//name
+		const std::string& _description,//description
+		const std::string& _units,//units	
+		const size_t& _bands,//number of bands
+		const nc_type& _ncstoragetype,//binary storage tyrpe
+		const std::string& _ncdimname,//dimension names		
+		const char& _notation,//ascii form I, F, E
+		const size_t& _width,//ascii width
+		const size_t& _precision//ascii numver of decimals places			
+	) {	
+		std::shared_ptr<cOutputField> f = getfield(_name);
+		if (!f) {
+			flist.push_back(cOutputField(_name, _description, _units, _bands, _ncstoragetype, _ncdimname, _notation, _width, _precision));
+			//f = std::make_shared<cOutputField>(flist.back());
+			f = &flist.back();
+			addvar(flist.back());			
+		}
+		//auto f = std::make_shared<cOutputField>(_name, _description, _units, _bands, _ncstoragetype, _ncdimname, _notation, _width, _precision);		
+		int i = f.use_count();
+		return f;
+	}
+	
+	void set_manager(cOutputManager* m) {
+
+	};
+
+	void begin_point_output() { };
+
+	void end_point_output() { };
+
+	bool addvar(cOutputField& of) {
+		if (NC.hasVar(of.name) == false) {
+			NcDim dim;
+			if (of.ncdimname.size()) {
+				dim = NC.addDim(of.ncdimname, of.bands);
+			}
+
+			of.var = std::make_shared<cSampleVar>(NC.addSampleVar(of.name, of.ncstoragetype, dim));
+			of.var->add_description(of.description);
+			of.var->add_units(of.units);
+		}
+		else {
+			if (NC.isLineVar(NC.getVar(of.name))) {
+				cLineVar v  = NC.getLineVar(of.name);
+				of.var = std::make_shared<cLineVar>(v);				
+			}
+			else if (NC.isSampleVar(NC.getVar(of.name))) {
+				cSampleVar v = NC.getSampleVar(of.name);
+				of.var = std::make_shared<cSampleVar>(v);
+			}
+				
+		}
+		return true;
+	}
+	
+	virtual bool write(const int& val, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, val);		
+	}
+	virtual bool write(const size_t& val, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, val);
+	}
+	virtual bool write(const float& val, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, val);
+	}
+	virtual bool write(const double& val, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, val);
+	}
+
+	virtual bool write(const std::vector<int>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, vals);		
+	}
+	virtual bool write(const std::vector<size_t>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, vals);
+	}
+	virtual bool write(const std::vector<float>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, vals);
+	}
+	virtual bool write(const std::vector<double>& vals, std::shared_ptr<cOutputField> of, const int& pointindex) {
+		return of->var->putRecord(pointindex, vals);		
+	}
+		
+};
+#endif
+
+#endif
+
+ 
