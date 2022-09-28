e28b99e44c892a8c417dbe294009fb630fc82fcb
Ross Brodie <rcb547@users.noreply.github.com>
2022-09-28 15:18:13 +1000

    Further changes to cmake for win10

--- cmake_build_script_win10.bat
+++ cmake_build_script_win10.bat
@@ -1,26 +1,28 @@
 @ECHO off
 
 rem %CommSpec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-SET Boost_ROOT=C:\Users\rossc\work\code\third_party\boost_1_79_0
-SET CGAL_ROOT=C:\Users\rossc\work\code\third_party\CGAL-5.4.1
-SET FFTW_ROOT=C:\Users\rossc\work\code\third_party\fftw3.2.2.dlls\64bit
-rem SET NETCDFCXX_ROOT=C:\Users\rossc\work\code\repos\ga-aem-csiro\submodules\netcdf-cxx4
+rem SET Boost_DIR=C:\Users\rossc\work\code\third_party\boost_1_79_0
+rem SET CGAL_DIR=C:\Users\rossc\work\code\third_party\CGAL-5.4.1
+SET FFTW_DIR=C:\Users\rossc\work\code\third_party\fftw3.2.2.dlls\64bit
+SET PETSC_DIR=C:\Users\rossc\work\code\third_party\petsc-installed\3.9.4\vs2017
+SET PETSC_INCLUDE_DIRS=%PETSC_DIR%\include;%PETSC_DIR%\win64_release\include
+SET PETSC_LIB_DIR=%PETSC_DIR%\win64_release\lib
+SET PETSC_LIBRARIES=libf2cblas.lib;libf2clapack.lib;libpetsc.lib
+SET PETSC_LDFLAGS=
 
-rem set PATH=%PATH%;C:\ProgramData\msys64\mingw64\bin;C:\ProgramData\msys64\usr\bin;D:\Software\ga-aem\third_party\fftw3.2.2.dlls\64bit
-rem set VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140
-
-rem set PATH=%PATH%;C:\ProgramData\msys64\mingw64\bin;C:\ProgramData\msys64\usr\bin;D:\Software\ga-aem\third_party\fftw3.2.2.dlls\64bit
-rem set VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140
+SET NETCDF_DIR=C:\Program Files\netCDF 4.6.1
+SET NETCDF_INCLUDE_DIR=%NETCDF_DIR%\include
+SET NETCDF_LIB_DIR=%NETCDF_DIR%\lib
+SET NETCDF_LIBRARIES=netcdf.lib;hdf5.lib;hdf5_hl.lib;libcurl_imp.lib
 
 set BUILD_DIR=%cd%\win10-build-vs2019
 set INSTALL_DIR=%cd%\win10-install-vs2019
 
 rem RMDIR /S /Q %BUILD_DIR%
-rem mkdir %BUILD_DIR%
+mkdir %BUILD_DIR%
 
 cd %BUILD_DIR%
 cmake -Wno-dev -G "Visual Studio 16 2019" -A x64 -DCMAKE_CXX_COMPILER=msvc ..
-rem cmake --build . --target libnetcdfcxx --config=Release
 rem cmake --build . --target galeisbstdem --config=Release
 rem cmake --build . --target garjmcmctdem --config=Release
 rem cmake --build . --target galeisbsfdem --config=Release
@@ -41,5 +43,6 @@ rem cmake --build . --target removelog10conductivityfromsgrid --config=Release
 rem cmake --build . --config=Release
 rem cmake --install . --prefix %INSTALL_DIR%
 
+cd ..
 PAUSE
 
