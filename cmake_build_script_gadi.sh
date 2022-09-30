bde07eb91bfa3104f7bf9ed51fb664b381203018
rcb547 <ross.c.brodie@icloud.com>
2022-09-30 20:07:44 +1000

    Improved cmake

---
+++ cmake_build_script_gadi.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+module load intel-compiler
+#module load gcc/11.1.0
+module load cmake/3.16.2
+module load fftw3/3.3.8
+module load netcdf/4.8.0
+module load gdal/3.0.2
+module load openmpi/4.1.4
+module load petsc/3.17.4
+module list 
+
+# Add the PETSc pkg-config path to the pkg-config search path
+export PKG_CONFIG_PATH=$PETSC_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
+
+# pkg-config for PETSc seems non-standard on gadi so add the ompi/compiler specific library directory 
+export PETSC_EXTRA_LIB_DIR=$PETSC_DIR/lib/ompi3/Intel
+#export PETSC_EXTRA_LIB_DIR=$PETSC_DIR/lib/ompi3/GNU
+
+export BUILD_DIR=$PWD/build-intel
+export INSTALL_DIR=$PWD/install-intel
+
+# build is a temporary directory for building (compiling and linking)
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+cmake -Wno-dev -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_BUILD_TYPE=Release  ..
+cmake --build . --target all
+
+#cmake --build . --target galeisbstdem
+#cmake --build . --target garjmcmctdem
+#cmake --build . --target galeisbsfdem
+#cmake --build . --target galeiallatonce
+
+#cmake --build . --target gaforwardmodeltdem
+#cmake --build . --target example_forward_model
+#cmake --build . --target example_forward_model_c
+#cmake --build . --target gatdaem1d-static
+#cmake --build . --target gatdaem1d-shared
+
+#cmake --build . --target ctlinedata2sgrid
+#cmake --build . --target ctlinedata2slicegrids
+#cmake --build . --target removelog10conductivityfromsgrid
+## Windows only ctlinedata2georefimage
+## Windows only ctlinedata2curtainimage
+
+#cmake --build .
+#cmake --install . --prefix $INSTALL_DIR
+
