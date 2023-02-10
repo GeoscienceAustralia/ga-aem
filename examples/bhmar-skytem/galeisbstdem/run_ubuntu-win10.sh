4f964a779f16925ccee16c9d0e63626a5e6952c0
Ross Brodie <rcb547@users.noreply.github.com>
2023-02-10 13:33:43 +1100

    Added a run script example

---
+++ examples/bhmar-skytem/galeisbstdem/run_ubuntu-win10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Standalone
+# ../../../bin/gnu/galeisbstdem.exe galeisbstdem.con
+
+# Use 4 MPI processes
+# mpirun -np 4 ../../../bin/gnu/galeisbstdem.exe galeisbstdem.con
+
+# Use 4 OpenMP threads
+# ../../../bin/gnu/galeisbstdem.exe galeisbstdem.con 4
+
