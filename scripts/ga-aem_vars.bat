fe2c84358e42c6c1da5aad04908abb596907aab5
Ross Brodie <rcb547@users.noreply.github.com>
2023-05-25 10:26:23 +0800

    Added scripts folder

---
+++ scripts/ga-aem_vars.bat
@@ -0,0 +1,11 @@
+@ECHO ---------------------------------------------------------------------------
+@ECHO Adding GA-AEM programs and dependencies to the PATH environment variable
+@ECHO ---------------------------------------------------------------------------
+
+@ECHO OFF
+REM Add executable and library directories to your search path
+REM Modify the paths below to suit your computer's folder structure
+set path=%GA-AEM_ROOT%\bin;%PATH%
+set path=C:\Program Files\netCDF 4.6.1\bin;%PATH%
+set path=%LocalAppData%\fftw-3.3.5-dll64;%path%
+set path=%LocalAppData%\gdal-3.0.4\bin;%path%
