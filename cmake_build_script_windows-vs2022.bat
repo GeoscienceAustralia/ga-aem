7599ad5377d5da7975a0cbc3dde45efbb8bff0f7
Ross Brodie <rcb547@users.noreply.github.com>
2023-05-25 10:24:43 +0800

    Added support for FFTW MSVC import library custom command in cmake Added scripts directory Installing scripts in install_dir

--- cmake_build_script_windows-vs2022.bat
+++ cmake_build_script_windows-vs2022.bat
@@ -2,72 +2,76 @@
 
 REM Run this script from the "X64 Native Tools Command Prompt for VS 2019 (or VS 2022)" so that all the compiler stuff is set up
 
-rem BUILD_DIR is a temporary directory for building (compiling and linking)
+REM BUILD_DIR is a temporary directory for building (compiling and linking)
 set BUILD_DIR=%cd%\build-windows-vs2022
-rem INSTALL_DIR is the directory for installing the built package and examples (e.g. c:\myprograms\ga-aem)
-set INSTALL_DIR=%cd%\install-windows-vs2022
 
-rem Set the FFTW path if necesary
-SET FFTW_DIR=C:\Users\rossc\work\code\third_party\fftw3.2.2.dlls\64bit
+REM Optionally delete the BUILD_DIR to ensure a clean cache/start
+REM DEL /S/Q %BUILD_DIR%
 
-rem Set the PETSc path if necesary
-SET PETSC_DIR=C:\Users\rossc\work\code\third_party\petsc-installed\3.9.4\vs2017
+REM INSTALL_DIR is the directory for installing the built package and examples (e.g. c:\myprograms\ga-aem)
+set INSTALL_DIR=%LocalAppData%\GA-AEM
+
+REM Set the FFTW path if necesary
+SET FFTW_DIR=%LocalAppData%\fftw-3.3.5-dll64
+
+REM Set the PETSc paths if necesary
+SET PETSC_DIR=%LocalAppData%\petsc\3.9.4\vs2017
 SET PETSC_INCLUDE_DIRS=%PETSC_DIR%\include;%PETSC_DIR%\win64_release\include
 SET PETSC_LIB_DIR=%PETSC_DIR%\win64_release\lib
 SET PETSC_LIBRARIES=libf2cblas.lib;libf2clapack.lib;libpetsc.lib
 SET PETSC_LDFLAGS=
 
-rem Set the NetCDF path if necesary
+REM Set the NetCDF path if necesary
 SET NETCDF_DIR=C:\Program Files\netCDF 4.6.1
 SET NETCDF_INCLUDE_DIR=%NETCDF_DIR%\include
 SET NETCDF_LIB_DIR=%NETCDF_DIR%\lib
 SET NETCDF_LIBRARIES=netcdf.lib;hdf5.lib;hdf5_hl.lib;libcurl_imp.lib
 
-rem Maybe delete the BUILD_DIR for a fresh start
-rem RMDIR /S /Q %BUILD_DIR%
+REM Maybe delete the BUILD_DIR for a fresh start
+REM RMDIR /S /Q %BUILD_DIR%
 
-rem Create and cd to the BUILD_DIR
+REM Create and cd to the BUILD_DIR
 mkdir %BUILD_DIR%
 cd    %BUILD_DIR%
 
-rem First generate the build cache first
-rem cmake -Wno-dev -G "Visual Studio 16 2019" -A x64 -DCMAKE_CXX_COMPILER=msvc ..
+REM First generate the build cache first
+REM cmake -Wno-dev -G "Visual Studio 16 2019" -A x64 -DCMAKE_CXX_COMPILER=msvc ..
 cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DCMAKE_CXX_COMPILER=msvc ..
 
-rem Switches for turning off certain dependencies if they are not wanted or available
-rem	-DWITH_MPI=OFF
-rem	-DWITH_NETCDF=OFF
-rem	-DWITH_GDAL=OFF
-rem	-DWITH_PETSC=OFF
-rem cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DCMAKE_CXX_COMPILER=msvc -DWITH_MPI=OFF -DWITH_NETCDF=OFF -DWITH_GDAL=OFF -DWITH_PETSC=OFF ..
+REM Switches for turning off certain dependencies if they are not wanted or available
+REM 	-DWITH_MPI=OFF
+REM 	-DWITH_NETCDF=OFF
+REM 	-DWITH_GDAL=OFF
+REM 	-DWITH_PETSC=OFF
+REM cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DCMAKE_CXX_COMPILER=msvc -DWITH_MPI=OFF -DWITH_NETCDF=OFF -DWITH_GDAL=OFF -DWITH_PETSC=OFF ..
 
-rem Build and install everything
+REM Build and install everything
 cmake --build . --config=Release
 cmake --install . --prefix %INSTALL_DIR%
 
-rem Or alternatively ...
+REM Or alternatively ...
 
-rem Build only particular targets
-rem cmake --build . --target galeisbstdem --config=Release
-rem cmake --build . --target garjmcmctdem --config=Release
-rem cmake --build . --target galeisbsfdem --config=Release
-rem cmake --build . --target galeiallatonce --config=Release
+REM Build only particular targets
+REM cmake --build . --target galeisbstdem --config=Release
+REM cmake --build . --target garjmcmctdem --config=Release
+REM cmake --build . --target galeisbsfdem --config=Release
+REM cmake --build . --target galeiallatonce --config=Release
 
-rem cmake --build . --target gaforwardmodeltdem --config=Release
-rem cmake --build . --target example_forward_model --config=Release
-rem cmake --build . --target example_forward_model_c --config=Release
-rem cmake --build . --target gatdaem1d-static --config=Release
+REM cmake --build . --target gaforwardmodeltdem --config=Release
+REM cmake --build . --target example_forward_model --config=Release
+REM cmake --build . --target example_forward_model_c --config=Release
+REM cmake --build . --target gatdaem1d-static --config=Release
 
-rem gatdaem1d-shared is required FOR python and matlab interfaces
-rem cmake --build . --target gatdaem1d-shared --config=Release  
+REM gatdaem1d-shared is required FOR python and matlab interfaces
+REM cmake --build . --target gatdaem1d-shared --config=Release  
 
-rem cmake --build . --target ctlinedata2sgrid --config=Release
-rem cmake --build . --target ctlinedata2slicegrids --config=Release
-rem cmake --build . --target ctlinedata2georefimage --config=Release
-rem cmake --build . --target ctlinedata2curtainimage --config=Release
-rem cmake --build . --target removelog10conductivityfromsgrid --config=Release
+REM cmake --build . --target ctlinedata2sgrid --config=Release
+REM cmake --build . --target ctlinedata2slicegrids --config=Release
+REM cmake --build . --target ctlinedata2georefimage --config=Release
+REM cmake --build . --target ctlinedata2curtainimage --config=Release
+REM cmake --build . --target removelog10conductivityfromsgrid --config=Release
 
-rem cmake --install . --prefix %INSTALL_DIR%
+REM cmake --install . --prefix %INSTALL_DIR%
 
 cd ..
 PAUSE
--- galei_vars.bat
+++
@@ -1,9 +0,0 @@
-@ECHO ---------------------------------------------
-@ECHO Adding GA-LEI directories to PATH env
-@ECHO ---------------------------------------------
-
-@ECHO OFF
-REM Add executable and library directories to your search path
-set path=.\bin\x64\Release\;%path%
-set path=C:\Program Files\fftw-3.2.2-dll64;%path%
-set path=C:\Program Files\netCDF 4.9.2\bin;%path%
