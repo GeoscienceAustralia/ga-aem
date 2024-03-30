f80199c2f9ed4b2cd1a089d32e88cd0724e91922
Ross Brodie <rcb547@users.noreply.github.com>
2024-03-30 16:25:24 +1100

    Added Tempest Lower Macquarie River examples

--- examples/Tempest-AusAEM-2020/galeisbstdem/run.bat
+++ examples/Tempest-AusAEM-2020/galeisbstdem/run.bat
@@ -8,20 +8,20 @@ REM Call "ga-aem_vars.bat" batch script to add executables and dependencies to y
 CALL %GA-AEM_ROOT%\scripts\ga-aem_vars.bat
 
 REM Run standalone on a single CPU
-REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
+REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of the total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components the total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
 
 REM Run using 4 OpenMP threads
-REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con 4		&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con 4	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
+REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con 4		&:: Invert amplitude of the total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con 4	&:: Invert X and Z-components the total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
 
 REM Use 4 MPI processes
-mpiexec -np 4 galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con	&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM mpiexec -np 4 galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
+mpiexec -np 4 galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con	&:: Invert amplitude of the total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM mpiexec -np 4 galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components the total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
 
 REM If you do not have MPI installed substitute galeisbstdem-nompi.exe in place of galeisbstdem.exe
-REM galeisbstdem-nompi.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM galeisbstdem-nompi.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
+REM galeisbstdem-nompi.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of the total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM galeisbstdem-nompi.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components the total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
 
 pause
 
--- examples/Tempest-Frome-2010/galeisbstdem/run.bat
+++ examples/Tempest-Frome-2010/galeisbstdem/run.bat
@@ -8,24 +8,24 @@ REM Call "ga-aem_vars.bat" batch script to add executables and dependencies to y
 CALL %GA-AEM_ROOT%\scripts\ga-aem_vars.bat
 
 REM Run standalone on a single CPU
-REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
-REM galeisbstdem.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con	&:: Invert only Z-component secondary field and do not solve for ant geometry
+REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
+REM galeisbstdem.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con	&:: Invert only Z-component secondary field and do not solve for any geometry parameters
 
 REM Run using 4 OpenMP threads
-REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con 4		&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con 4	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
-REM galeisbstdem.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con 4	&:: Invert only Z-component secondary field and do not solve for ant geometry
+REM galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con 4		&:: Invert amplitude of total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con 4	&:: Invert X and Z-components total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
+REM galeisbstdem.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con 4	&:: Invert only Z-component secondary field and do not solve for any geometry parameters
 
 REM Use 4 MPI processes
-mpiexec -np 4 galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM mpiexec -np 4 galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
-REM mpiexec -np 4 galeisbstdem.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con	&:: Invert only Z-component secondary field and do not solve for ant geometry
+mpiexec -np 4 galeisbstdem.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM mpiexec -np 4 galeisbstdem.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
+REM mpiexec -np 4 galeisbstdem.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con	&:: Invert only Z-component secondary field and do not solve for any geometry parameters
 
 REM If you do not have MPI installed substitute galeisbstdem-nompi.exe in place of galeisbstdem.exe
-REM galeisbstdem-nompi.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and do not solve Dx and Dz Tx-Rx offsets
-REM galeisbstdem-nompi.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve Dx and Dz Tx-Rx offsets and the Rx pitch
-REM galeisbstdem-nompi.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con	&:: Invert only Z-component secondary field and do not solve for ant geometry
+REM galeisbstdem-nompi.exe galeisbstdem-xzamplitude-solve-offsets.con			&:: Invert amplitude of total-field data in the X&Z-component plane and solve for the Dx and Dz Tx-Rx offsets
+REM galeisbstdem-nompi.exe galeisbstdem-xzcomponents-solve-offsets-and-rxpitch.con	&:: Invert X and Z-components total-field data and solve for the Dx and Dz Tx-Rx offsets and the Rx pitch
+REM galeisbstdem-nompi.exe galeisbstdem-zcomponentonly-do-not-solve-geometry.con	&:: Invert only Z-component secondary field and do not solve for any geometry parameters
 
 pause
 
