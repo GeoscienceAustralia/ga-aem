4043abba510ad1fab7045e5e678c49816a6a4c9f
Ross Brodie <rcb547@users.noreply.github.com>
2024-04-03 12:05:08 +1100

    Changed script names

---
+++ scripts/test_ga-aem_paths.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Reset the path to the very minimum
+set path=C:\Windows\System32
+
+REM Ideally you would set GA-AEM_ROOT as a variable in your user environment (e.g. Start | Edit environment variable for your account)
+set GA-AEM_ROOT=%LocalAppData%\GA-AEM
+
+REM Call batch script to setup the GA-AEM programs and dependency paths
+CALL %GA-AEM_ROOT%\scripts\ga-aem_vars.bat
+
+ECHO "-----------------"
+galeisbstdem.exe
+ECHO "-----------------"
+galeiallatonce.exe
+ECHO "-----------------"
+ctlinedata2sgrid.exe
+ECHO "-----------------"
+ctlinedata2slicegrids.exe
+ECHO "-----------------"
+
+pause
+
+
